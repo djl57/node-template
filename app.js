9b5ae05bc0c552bf8be9466035f48b2002eb6dd3
djl57 <18367345931@163.com>
2019-07-17 17:12:13 +0800

    co

--- app.js
+++ app.js
@@ -11,11 +11,17 @@ app.use(bodyParser())
 // 引入接口
 const planRouter = require('./interface/plan')
 const userRouter = require('./interface/user')
+const testRouter = require('./interface/test')
+const waterRouter = require('./interface/water')
 
 app.use(planRouter.routes())
 app.use(planRouter.allowedMethods())
+app.use(testRouter.routes())
+app.use(testRouter.allowedMethods())
 app.use(userRouter.routes())
 app.use(userRouter.allowedMethods())
+app.use(waterRouter.routes())
+app.use(waterRouter.allowedMethods())
 
 app.listen(8888, () => {
   console.log('http://localhost:8888')
---
+++ interface/test.js
@@ -0,0 +1,18 @@
+const router = require('koa-router')()
+const DB = require('../dbs/db')
+const collectionName = 'cover'
+const collectionName2 = 'plan'
+
+router.post('/cover', async ctx => {
+  console.log(ctx.url)
+  const result = ctx.request.body
+  result.status = 0
+  const insert = await DB.insert(collectionName, result)
+  if (insert.result.ok === 1) {
+    ctx.body = { code: 0, msg: '封面插入成功', }
+  } else {
+    ctx.body = { code: 1, msg: '封面插入失败' }
+  }
+})
+
+module.exports = router
